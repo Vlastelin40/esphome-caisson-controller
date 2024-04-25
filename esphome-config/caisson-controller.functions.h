7c8a48855591f8d2ae77fa539378e7d965cded14
false-coder <82243286+false-coder@users.noreply.github.com>
2024-04-25 13:53:05 +0300

    Изменён алгоритм для числа включений насоса в час
    Ранее подсчёт числа включений производился за фиксированный час, и каждый час счётчик сбрасывался. Теперь подсчёт числа включений ведётся в скользящем часовом окне от текущего включения насоса.

--- esphome-config/caisson-controller.functions.h
+++ esphome-config/caisson-controller.functions.h
@@ -1,4 +1,7 @@
-std::string seconds_to_string_dhms(uint32_t seconds) {
+#include <esphome.h>
+
+std::string seconds_to_string_dhms(uint32_t seconds) 
+{
 
       int days = 0;
       int hours = 0;
@@ -20,7 +23,8 @@ std::string seconds_to_string_dhms(uint32_t seconds) {
       return {buffer};
 }
 
-std::string seconds_to_string_hms(uint32_t seconds) {
+std::string seconds_to_string_hms(uint32_t seconds) 
+{
 
       int hours = 0;
       int minuts = 0;
@@ -35,4 +39,32 @@ std::string seconds_to_string_hms(uint32_t seconds) {
       char buffer[10];
       sprintf(buffer, "%u:%02u:%02u", hours, minuts, seconds);
       return {buffer};
+}
+
+int check_count_of_pump_starts()
+{
+    int len = sizeof(id(gs_pump_starts)) / sizeof(id(gs_pump_starts)[0]);                   // Длина массива моментов времени пусков насоса
+    int count = 0;                                                                          // Счётчик пусков за последний час
+    for (int i = len - 1; i > 0; i--) 
+        {
+            id(gs_pump_starts)[i] = id(gs_pump_starts)[i - 1];                              // Сдвижка элементов вправо
+        }
+    id(gs_pump_starts)[0] = millis();                                                       // Сохранение момента времени текущего пуска
+    for (int i = 1; i < len; i++) 
+        {
+            if (id(gs_pump_starts)[0] >= id(gs_pump_starts)[i])                             // Если millis() не переполнился
+            {
+                if (id(gs_pump_starts)[0] - id(gs_pump_starts)[i] < 3600000)                // Если дельта менее часа
+                {
+                    count++;
+                }
+            } else                                                                          // Если millis() переполнился
+            {
+                if (id(gs_pump_starts)[0] + 4294967295 - id(gs_pump_starts)[i] < 3600000)   // Если дельта менее часа
+                {
+                    count++;
+                }
+            }
+        }
+    return {count};
 }
